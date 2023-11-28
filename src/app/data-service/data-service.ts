f9f5cac41092cdafe379372a92004158c87a0a54
Govindagowda Pramilamma Shreyas <g_shreyas@la.jobtkk.co.jp>
2023-11-28 11:53:54 +0900

    /add and /get

--- src/app/data-service/data-service.ts
+++ src/app/data-service/data-service.ts
@@ -1,17 +1,36 @@
-// src/app/person.service.ts
+// data.service.ts
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Person {
+  id?: number;
+  name: string;
+  age: number;
+  email: string;
+}
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PersonService {
-  private apiUrl = 'http://localhost:7080';  // Update with your Flask API endpoint
+  private apiUrl = 'http://localhost:7080'; // Update with your Flask server URL
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  addPerson(personData: any): Observable<any> {
-    return this.http.post(`${this.apiUrl}/add_person`, personData);
+  addPerson(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/add_person`, data);
+  }
+
+  getPerson(personId: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/get_person/${personId}`);
+  }
+
+  editPerson(personId: number, data: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/edit_person/${personId}`, data);
+  }
+
+  deletePerson(personId: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/delete_person/${personId}`);
   }
 }
--- src/app/home-container/home-container.component.ts
+++ src/app/home-container/home-container.component.ts
@@ -1,34 +1,79 @@
 // src/app/home-container.component.ts
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PersonService } from '../data-service/data-service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home-container',
   templateUrl: './home-container.component.html',
   styleUrls: ['./home-container.component.css']
 })
-export class HomeContainerComponent {
+export class HomeContainerComponent implements OnInit {
   personData = {
     name: '',
-    age: null,
+    age: null as number | null, // Update type definition for age
     email: ''
   };
 
+  // Subscription to keep track of the observable subscription
+  private addPersonSubscription: Subscription | undefined;
+
   constructor(private personService: PersonService) { }
 
-  addPerson() {
-    this.personService.addPerson(this.personData).subscribe(response => {
-      console.log(response);
-      // Handle success or update your UI accordingly
-      // Clear the input fields after success
-      this.personData = {
-        name: '',
-        age: null,
-        email: ''
-      };
-    }, error => {
-      console.error(error);
-      // Handle error
+  ngOnInit() {
+    // Example: Log the value returned by getPerson for person with ID 1
+    this.personService.getPerson(3).subscribe({
+      next: person => {
+        console.log('Person data:', person);
+      },
+      error: error => {
+        console.error('Error getting person:', error);
+      }
     });
+  }
+
+  // Function to add a new person
+  addPerson() {
+    // Validate input data
+    if (!this.isValidInput()) {
+      console.error('Invalid input data');
+      // Provide user feedback about the invalid input
+      return;
+    }
+
+    // Unsubscribe from the previous subscription if it exists
+    if (this.addPersonSubscription) {
+      this.addPersonSubscription.unsubscribe();
+    }
+
+    // Call the service to add a person
+    this.addPersonSubscription = this.personService.addPerson(this.personData).subscribe({
+      next: response => {
+        console.log(response);
+        // Handle success: Update UI, show success message, etc.
+        // Clear the input fields after success
+        this.clearInputFields();
+      },
+      error: error => {
+        console.error(error);
+        // Handle error: Show user-friendly error message
+      }
+    });
+  }
+
+  // Function to validate input data
+  private isValidInput(): boolean {
+    return this.personData.name.trim() !== '' &&
+           this.personData.email.trim() !== '' &&
+           !isNaN(this.personData.age as number); // Type assertion for age
+  }
+
+  // Function to clear input fields
+  private clearInputFields(): void {
+    this.personData = {
+      name: '',
+      age: null,
+      email: ''
+    };
   }
 }
